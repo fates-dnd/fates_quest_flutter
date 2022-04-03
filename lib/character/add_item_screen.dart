f974a5f68950a6f54cb3d8c25208cd9fd4362ca8
David Leibovych <ariedov@gmail.com>
2022-04-03 12:55:03 +0300

    fixes items population

--- lib/character/add_item_screen.dart
+++ lib/character/add_item_screen.dart
@@ -2,15 +2,22 @@ import 'package:fates_quest_flutter/character/roll_outcomes_form.dart';
 import 'package:fates_quest_flutter/data/character.dart';
 import 'package:fates_quest_flutter/data/item.dart';
 import 'package:fates_quest_flutter/data/roll_outcome.dart';
+import 'package:fates_quest_flutter/model/character_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:provider/provider.dart';
 
 class AddItemScreen extends StatefulWidget {
   final Character character;
+  final int position;
   final Item? item;
 
-  const AddItemScreen({Key? key, required this.character, this.item})
-      : super(key: key);
+  const AddItemScreen({
+    Key? key,
+    required this.character,
+    required this.position,
+    this.item,
+  }) : super(key: key);
 
   @override
   State<StatefulWidget> createState() {
@@ -100,14 +107,14 @@ class _AddItemScreenState extends State<AddItemScreen> {
             ),
             ElevatedButton(
               onPressed: () {
-                // Provider.of<CharacterModel>(context, listen: false)
-                //     .addItemToCharacter(
-                //   widget.character,
-                //   Item()
-                //     ..name = nameController.text
-                //     ..description = descriptionController.text
-                //     ..variableOutcome = rollOutcomes,
-                // );
+                Provider.of<CharacterModel>(context, listen: false).setItemAt(
+                  widget.character,
+                  widget.position,
+                  Item()
+                    ..name = nameController.text
+                    ..description = descriptionController.text
+                    ..variableOutcome = rollOutcomes,
+                );
 
                 Navigator.of(context).pop();
               },
--- lib/character/items_list.dart
+++ lib/character/items_list.dart
@@ -1,7 +1,7 @@
+import 'package:fates_quest_flutter/character/add_item_screen.dart';
 import 'package:fates_quest_flutter/data/character.dart';
 import 'package:fates_quest_flutter/data/item.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class ItemList extends StatelessWidget {
   final Character character;
@@ -10,16 +10,17 @@ class ItemList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final localization = AppLocalizations.of(context)!;
-
     return Column(
       children: [
         Expanded(
           child: ListView.separated(
             itemCount: 12,
-            separatorBuilder: (context, index) => const SizedBox(height: 16),
-            itemBuilder: (context, index) =>
-                ItemRow(item: character.items[index]),
+            separatorBuilder: (context, index) => const SizedBox(height: 8),
+            itemBuilder: (context, index) => ItemRow(
+              character: character,
+              index: index,
+              item: character.items[index],
+            ),
           ),
         ),
         const SizedBox(height: 32),
@@ -29,23 +30,58 @@ class ItemList extends StatelessWidget {
 }
 
 class ItemRow extends StatelessWidget {
+  final Character character;
+  final int index;
   final Item? item;
 
-  const ItemRow({Key? key, this.item}) : super(key: key);
+  const ItemRow(
+      {Key? key, required this.character, required this.index, this.item})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        border: Border.all(color: Colors.black),
-      ),
-      padding: const EdgeInsets.all(16),
-      child: Row(
-        children: [
-          Text(item?.name ?? ""),
-        ],
-      ),
+    return Stack(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 8, bottom: 8),
+          child: InkWell(
+            onTap: () => Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => AddItemScreen(
+                      character: character,
+                      position: index,
+                      item: item,
+                    ))),
+            borderRadius: BorderRadius.circular(8),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                border: Border.all(color: Colors.black),
+              ),
+              padding: const EdgeInsets.all(16),
+              child: Row(
+                children: [
+                  Text(item?.name ?? ""),
+                ],
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          child: Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.black,
+              border: Border.all(color: Colors.black),
+            ),
+            padding: const EdgeInsets.all(5),
+            child: Text(
+              "${index + 1}",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
